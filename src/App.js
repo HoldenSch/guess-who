05e8d1d91befa24aeb215935f77d1e5f679eca89
Holden Schermer <holden@Holdens-MacBook-Pro.local>
2023-11-28 18:57:58 -0500

    fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-<<<<<<< HEAD
-//import Navbar from "./Components/Navbar/Navbar.js";
-=======
->>>>>>> abb9061cf5650b5ce2f1d1661c3edc8f999f1e41
 import { BrowserRouter as Router, Routes, Route,} from "react-router-dom";
 import Register from './Components/Register.js';
 import Login from './Components/Login.js';
 import Play from './Components/Play.js';
+import Home from './Components/Play.js';
+ 
  
 function App() {
     return (
@@ -14,11 +12,8 @@ function App() {
             <Routes>
                 <Route path='/' element={<Login />}></Route>
                 <Route path='/register' element={<Register />}></Route>
-<<<<<<< HEAD
                 <Route path='/play' element={<Play />}></Route>
-=======
                 <Route path='/home' element={<Home />}></Route>
->>>>>>> abb9061cf5650b5ce2f1d1661c3edc8f999f1e41
             </Routes>
         </Router>
     );
