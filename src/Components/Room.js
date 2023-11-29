66a4de99e6377b01c921c5948bde2f84ce368404
Holden Schermer <holden@dhcp-10-250-73-21.harvard.edu>
2023-11-29 08:09:13 -0500

    drag n drop

--- src/Components/Room.js
+++ src/Components/Room.js
@@ -1,38 +1,12 @@
 import React from "react";
-import { IoIosSettings } from "react-icons/io";
-import "./Play.css";
+//import { IoIosSettings } from "react-icons/io";
+//import "./Play.css";
+import DragList from "./ReactComponents/DragList";
+import ReactDOM from "react-dom";
 
 const Room = () => {
-  return (
-    <div class="fullPage">
-      <h1>
-        <IoIosSettings class="settingsButton" />
-      </h1>
-      <body>
-        <div class="gameName">
-          <p>Guess Who!</p>
-        </div>
-        <div class="startMenu">
-          <form class="enterCode">
-            <div class="fullGame">
-            <input
-              type="text"
-              id="gameCode"
-              name="gameCode"
-              placeholder="Enter Game Code"
-            ></input>
-            <input class="joinButton" type="submit" value="Join"></input>
-            </div>
-          </form>
-          <div class="createRoom d-grid gap-2 col-11 mx-auto" >
-            <button class="btn btn-primary" type="button">
-              Create Private Room
-            </button>
-          </div>
-        </div>
-      </body>
-    </div>
-  );
+    const rootElement = document.getElementById("root");
+    ReactDOM.render(<DragList />, rootElement);
 };
 
 export default Room;
