3a82de1b2b1bca418730a59e0d22cd95a7e7486f
Holden Schermer <holden@dhcp-10-250-73-21.harvard.edu>
2023-11-30 21:08:02 -0500

    board screen

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,14 +1,54 @@
 import React from "react";
 import "./Play.css";
-import CustomCard from "./ReactComponents/Card";
+import FlipCard from "./ReactComponents/FlipCard";
 import dogImage from "./Images/dog.jpeg";
+import redX from "./Images/redX.jpeg";
 
 const Board = () => {
+  // Array of objects representing flip cards
+  const flipCards = [
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    { frontImage: dogImage, frontTitle: "Front Title 1", backImage: redX, backTitle: "" },
+    
+   
+    // Add more objects as needed
+  ];
+
   return (
-    <div class="fullPage">
-    <body>
-    <CustomCard imageSrc={dogImage} title="Card Title"></CustomCard>
-    </body>
+    <div className="fullPage">
+      <div className="fullBoard"> 
+      {flipCards.map((card, index) => (
+        <FlipCard 
+          key={index}
+          frontImage={card.frontImage}
+          frontTitle={card.frontTitle}
+          backImage={card.backImage}
+          backTitle={card.backTitle}
+        />
+      ))}
+      </div>
     </div>
   );
 };
---
+++ src/Components/ReactComponents/FlipCard.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import './FlipCard.css'; // Make sure this CSS file is linked
+
+const FlipCard = ({ frontImage, frontTitle, backImage, backTitle }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  return (
+    <div className={`flip-card ${isFlipped ? 'flipped' : ''}`} onClick={handleClick}>
+      <div className="flip-card-inner">
+        <div className="flip-card-front">
+          <Card>
+            <Card.Img variant="top" src={frontImage} className="card-img-custom" />
+            <Card.Body>
+              {/* Apply the custom class to your titles */}
+              <Card.Title className="card-title-custom">{frontTitle}</Card.Title>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="flip-card-back">
+          <Card>
+            <Card.Img variant="top" src={backImage} className="card-img-custom" />
+            <Card.Body>
+              {/* Apply the custom class to your titles */}
+              <Card.Title className="card-title-custom">{backTitle}</Card.Title>
+            </Card.Body>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FlipCard;
