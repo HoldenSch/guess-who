77d3140281764d2e0eee4e152334659edf6f6156
Holden Schermer <holden@dhcp-10-250-73-21.harvard.edu>
2023-12-02 03:02:33 -0500

    Asdjasd

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,15 @@ const express = require("express");
 const mysql = require('mysql');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
+const multer = require('multer');
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Setup multer for file handling
+const upload = multer({ dest: 'uploads/' });
+
 let session_id = 0;
 
 // creates connection with MySQL
@@ -20,37 +25,27 @@ const db = mysql.createConnection({
 app.post('/validate', (req, res) => {
     const sql = "SELECT * FROM `guess-who-database`.users WHERE `username` = ?";
     db.query(sql, [req.body.username], (err, data) => {
-        // catches error when selecting
         if (err) {
             return res.json("Error");
-        }
-        // if 'SELECT' statement returns rows, username is taken
-        else if (data.length > 0) {
+        } else if (data.length > 0) {
             return res.json("Username taken");
         }
-        // otherwise, username is not taken
         return res.json("Success");
     });
 });
 
 // registers the user
 app.post('/register', (req, res) => {
-    // generates salt encryption
     bcrypt.genSalt(10, (err1, salt) => {
-        // catches error generating salt
         if (err1) {
           return res.json("Error");
         }
-        // hashes password with salt
         bcrypt.hash(req.body.password.toString(), salt, (err2, hash) => {
-            // catches error hashing
             if (err2) {
               return res.json("Error");
             }
-            // inserts username and hash into table
             const sql = "INSERT INTO `guess-who-database`.users (`username`, `password_hash`) VALUES (?, ?);";
             db.query(sql, [req.body.username, hash], (err3, data) => {
-                // catches error when inserting
                 if (err3) {
                     return res.json("Error");
                 }
@@ -64,41 +59,37 @@ app.post('/register', (req, res) => {
 app.post('/login', (req, res) => {
     const sql = "SELECT * FROM `guess-who-database`.users WHERE `username` = ?";
     db.query(sql, [req.body.username], (err1, data) => {
-        // catches error when selecting
         if (err1 || data.length === 0) {
             return res.json("Error");
         }
-        // compares the password to the stored password hash
         bcrypt.compare(req.body.password.toString(), data[0].password_hash, (err2, result) => {
-            // catches error when comparing passwords
             if (err2) {
                 return res.json("Error");
             }
-            // logs user in if passwords match
             if (result) {
-                // sets session id to the user id
                 session_id = data[0].id;
                 return res.json('Success');
-            } 
-            // otherwise passwords do not match
-            else {
+            } else {
                 return res.json("Fail");
             }
         });
     })
 });
 
-// inserts name for user
-app.post('/insert', (req, res) => {
+// Modified /insert route to handle file upload
+app.post('/insert', upload.single('image'), (req, res) => {
     if (session_id === 0) {
         return res.json("Not Logged In");
     }
-    const sql = "INSERT INTO `guess-who-database`.names (user_id, name) VALUES (?, ?);";
+
+    const sql = "INSERT INTO `guess-who-database`.names (user_id, name, image) VALUES (?, ?, ?);";
     db.query(sql, [session_id, req.body.content], (err1, data) => {
-        // catches error when inserting
         if (err1) {
             return res.json("Error");
         }
+
+        // Handle the uploaded file here (if required)
+
         console.log(data.insertId);
         return res.json(data.insertId);
     });
@@ -111,7 +102,6 @@ app.post('/delete', (req, res) => {
     }
     const sql = "DELETE FROM `guess-who-database`.names WHERE id = ?;";
     db.query(sql, [req.body.id], (err1, data) => {
-        // catches error when deleting
         if (err1) {
             console.log(err1);
             return res.json("Error");
@@ -127,15 +117,13 @@ app.post('/retrieve', (req, res) => {
     }
     const sql = "SELECT * FROM `guess-who-database`.names WHERE user_id = ?;";
     db.query(sql, [session_id], (err1, data) => {
-        // catches error when deleting
         if (err1) {
-            return res.json("Not Logged In");
+            return res.json("Error");
         }
         return res.json(data);
     });
 });
 
-// tells app to listen to port 8081
 app.listen(8081, ()=> {
     console.log("listening");
 });
