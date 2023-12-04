11bad1c3b0808e6dac82010199971edaa398c100
SUNNY LIU <SUNNYLIU@COLLEGE.HARVARD.EDU>
2023-12-04 15:41:22 -0500

    fixed max payload message

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const bcrypt = require('bcryptjs');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '10mb'}));
 let session_id = 0;
 
 // creates connection with MySQL
